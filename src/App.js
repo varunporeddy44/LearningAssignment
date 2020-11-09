df81957e55db4e2f8c121b305250f3e69ff60d28
Varun Poreddy <varunreddyporeddy44@gmail.com>
2020-11-09 18:53:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Data from "./data.json"
 
 function App(){
   const colors=["red","blue","orange","green","grey","yellow"]
+  var n=0
+  var a=0
   return(
     <div>
       {Data.map((post,i)=>{
+        
+        a=i%colors.length
+        n=(i-a)/colors.length
         return(
-          
         <div style={{fontSize:post.count,textAlign:"center",
           color:
           i>=colors.length 
-          ?colors[i-colors.length]
+          ?colors[i-(n*colors.length)]
           :colors[i]
         }}
+        onClick={()=>{alert(n)}}
         key={i}>{post.keyword}</div>
         )
       })}
